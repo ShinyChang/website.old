fc5b0bafe42ce21c13aba436bbdac508facbaac0
ZhiHong Chang <ublue724@gmail.com>
2014-04-29 21:49:43 +0800

    do not display uncompleted article in list

--- modals/articleProvider.js
+++ modals/articleProvider.js
@@ -1,8 +1,8 @@
-var Db         = require('mongodb').Db;
+var Db = require('mongodb').Db;
 var Connection = require('mongodb').Connection;
-var Server     = require('mongodb').Server;
-var BSON       = require('mongodb').BSON;
-var ObjectID   = require('mongodb').ObjectID;
+var Server = require('mongodb').Server;
+var BSON = require('mongodb').BSON;
+var ObjectID = require('mongodb').ObjectID;
 
 ArticleProvider = function(host, port) {
     this.db = new Db('mydb', new Server(host, port, {
@@ -24,7 +24,11 @@ ArticleProvider.prototype.findPage = function(page, callback) {
     this.getCollection(function(error, article_collection) {
         if (error) callback(error)
         else {
-            article_collection.find().skip((page - 1) * 10).limit(10).sort({
+            article_collection.find({
+                createTime: {
+                    $exists: true
+                }
+            }).skip((page - 1) * 10).limit(10).sort({
                 _id: -1
             }).toArray(function(error, results) {
                 if (error) callback(error)
@@ -38,7 +42,9 @@ ArticleProvider.prototype.findTag = function(tag, callback) {
     this.getCollection(function(error, article_collection) {
         if (error) callback(error)
         else {
-            article_collection.find({tag: tag}).sort({
+            article_collection.find({
+                tag: tag
+            }).sort({
                 _id: -1
             }).toArray(function(error, results) {
                 if (error) callback(error)
@@ -66,8 +72,8 @@ ArticleProvider.prototype.count = function(callback) {
     this.getCollection(function(error, article_collection) {
         if (error) callback(error)
         else {
-            return article_collection.count(function(err, count){
-              callback(count);
+            return article_collection.count(function(err, count) {
+                callback(count);
             });
         }
     });
@@ -92,7 +98,7 @@ ArticleProvider.prototype.save = function(articles, callback) {
         if (error) callback(error)
         else {
             if (typeof articles.id !== "undefined") {
-                articles.lastUpdate = new Date;
+                articles.lastUpdate = new Date();
                 articles.createTime = article_collection.db.bson_serializer.ObjectID(articles.id).getTimestamp();
                 article_collection.update({
                     _id: article_collection.db.bson_serializer.ObjectID(articles.id)
@@ -100,7 +106,7 @@ ArticleProvider.prototype.save = function(articles, callback) {
                     callback(null, articles);
                 });
             } else {
-                articles.lastUpdate = new Date;
+                articles.lastUpdate = new Date();
                 article_collection.insert(articles, function() {
                     callback(null, articles);
                 });
