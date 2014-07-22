b5d8e3e2b13e7e731e8dc2cd9463647748819cc1
ZhiHong Chang <ublue724@gmail.com>
2014-07-22 22:56:07 +0800

    Update to newest dependencies

--- app.js
+++ app.js
@@ -79,7 +79,10 @@ app.set('view engine', 'ejs');
 app.use(logger({stream: logFile}));
 app.use(cookieParser(config.secret));
 app.use(session());
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json())
 app.use(csrf());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
