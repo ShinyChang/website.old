59a1b49568fe84710388e4923bf69992b88901d7
ShinyChang <ublue724@gmail.com>
2014-08-06 09:46:54 -0400

    missing shinychang.net

--- run.js
+++ run.js
@@ -4,4 +4,5 @@ var app = module.exports = express();
 
 app.use(vhost('github.shinychang.net', require('./github.js')));
 app.use(vhost('*.shinychang.net', require('./blog.js')));
+app.use(vhost('shinychang.net', require('./blog.js')));
 app.listen(80);
