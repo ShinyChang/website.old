c05ec9fe1238570b079894fbf80cdab28144651e
ZhiHong Chang <ublue724@gmail.com>
2014-10-25 17:13:19 +0800

    Integrate instantclick.js, addthis.js

--- locals/lang.js
+++ locals/lang.js
@@ -16,7 +16,7 @@ exports.lang = {
     signIn: '登入',
     about: '關於',
     article: '文章',
-    archive: '封存',
+    archive: '所有文章',
     logout: '登出',
     backToTop: '回到最上面',
     tagList: '標籤列表',
