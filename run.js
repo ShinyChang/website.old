fb3d0ee0e594a54ae2c8b8b7c7689c4751ddfc1e
ZhiHong Chang <ublue724@gmail.com>
2015-01-24 14:19:32 +0800

    wedding API (for testing)

--- run.js
+++ run.js
@@ -3,6 +3,7 @@ var vhost = require('vhost');
 var app = module.exports = express();
 
 app.use(vhost('github.shinychang.net', require('./github.js')));
+app.use(vhost('wedding.shinychang.net', require('./wedding.js')));
 app.use(vhost('*.shinychang.net', require('./blog.js')));
 app.use(vhost('shinychang.net', require('./blog.js')));
 app.listen(80);
