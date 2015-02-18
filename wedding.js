b3b1f60272111375c76b7845b771ccea4571039c
ZhiHong Chang <ublue724@gmail.com>
2015-02-18 20:03:03 +0800

    update wedding api

--- wedding.js
+++ wedding.js
@@ -9,17 +9,19 @@ var comments = JSON.parse(fs.readFileSync('_comments.json'));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 
 app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
 });
 
 app.get('/comments.json', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(comments));
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(comments));
 });
 
 app.post('/comments.json', function(req, res) {
@@ -31,6 +33,31 @@ app.post('/comments.json', function(req, res) {
     res.send(JSON.stringify(comments));
 });
 
+app.get('/reward', function(req, res) {
+    var rewardList = [];
+    var keyMap = {};
+    for (var i = comments.length - 1; i >= 0; i--) {
+        if (typeof keyMap[comments[i].id] === 'undefined') {
+            rewardList.push(comments[i]);
+            keyMap[comments[i].id] = rewardList.length - 1;
+            rewardList[rewardList.length - 1].count = 1;
+        } else {
+            rewardList[Object.keys(keyMap).indexOf(comments[i].id)].count++;
+        }
+    };
+
+    // shuffle
+    for (var i = rewardList.length - 1; i >= 0; i--) {
+
+        var randomIndex = Math.floor(Math.random() * (i + 1));
+        var itemAtIndex = rewardList[randomIndex];
+
+        rewardList[randomIndex] = rewardList[i];
+        rewardList[i] = itemAtIndex;
+    }
+    rewardList = rewardList.slice(0, 5);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(rewardList));
+});
+
 module.exports = app;
-
-
